d68f8047563bb2860e68bfd94ec79ec2b17ad337
Christophe Gardenat <christophe.gardenat@sage.com>
2026-04-03 14:02:51 +0200

    Add configuration settings for RGB lighting and combos

---
+++ config.h
@@ -0,0 +1,25 @@
+
+#pragma once
+
+//#define QUICK_TAP_TERM 0
+//#define TAPPING_TERM 100
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+//#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+
+#define SPLIT_LAYER_STATE_ENABLE
+
+
+#define U_MRDO SCMD(KC_Y)
+#define U_MPST LCMD(KC_V)
+#define U_MCPY LCMD(KC_C)
+#define U_MCUT LCMD(KC_X)
+#define U_MUND LCMD(KC_Z)
+#define U_WRDO C(KC_Y)
+#define U_WPST C(KC_V)
+#define U_WCPY C(KC_C)
+#define U_WCUT C(KC_X)
+#define U_WUND C(KC_Z)
+
+#define COMBO_COUNT 5
+#define COMBO_TERM 150
